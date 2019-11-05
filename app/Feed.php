0a68bcb7bebbc6f7beef5fcb3c487374660c038d
Aldi Satrio Aji <aldis85@yahoo.com>
2019-11-05 14:25:40 +0700

    if usr not null

--- app/Feed.php
+++ app/Feed.php
@@ -13,7 +13,7 @@ class Feed extends Model {
     public function getCreatedAtAttribute()
     {
         return \Carbon\Carbon::parse($this->attributes['created_at'])
-            ->format('H:i d-M-Y );
+            ->format('H:i d-M-Y');
     }
 
 //    protected $fillable = [];
