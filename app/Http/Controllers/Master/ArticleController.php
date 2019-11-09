0bbe11bc7db0875c7dc4a1dc5c7d29a3060e7de7
AldiSatrioAji <aldis85@yahoo.com>
2019-11-09 15:39:59 +0700

    article createdAt format

--- app/Http/Controllers/Master/ArticleController.php
+++ app/Http/Controllers/Master/ArticleController.php
@@ -26,13 +26,14 @@ class ArticleController extends Controller
 
     public function all(Request $request) {
         $data = $this->article;
-        if($request->has('take')) {
-            $data = $data->take($request->take)->get();
-            return Response::json($data, 200);
-        }
 
         if($request->has('orderBy')) {
             $data = $data->orderBy($request->orderBy, 'desc');
+        }
+
+        if($request->has('take')) {
+            $data = $data->take($request->take)->get();
+            return Response::json($data, 200);
         }
 
         $data = $data->paginate($request->per_page);
--- app/Http/Controllers/Master/FeedController.php
+++ app/Http/Controllers/Master/FeedController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Response;
 
-class FeedController
+class FeedController extends Controller
 {
     private $feed;
 
