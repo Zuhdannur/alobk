c836706a69e38bf784aa5f321f1a83fe55f3c2a9
AldiSatrioAji <aldis85@yahoo.com>
2019-11-17 16:06:54 +0700

    update

--- app/Http/Controllers/Guru/ScheduleController.php
+++ app/Http/Controllers/Guru/ScheduleController.php
@@ -122,13 +122,7 @@ class ScheduleController extends Controller
         );
 
         return Response::json([
-            'requester_id' => $update->requester_id,
-            'consultant_id' => $update->consultant_id,
-            'id' => $id,
-            'title' => $update->title,
-            'desc' => $update->desc,
-            'type_schedule' => $result->type_schedule,
-            'time' => $result->time,
+            'data' => $update,
             'message' => 'Pengajuan berhasil diterima.'
         ], 200);
     }
