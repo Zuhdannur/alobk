397f5a81b308f8b6180f2531100b52a2a8e8f2f1
AldiSatrioAji <aldis85@yahoo.com>
2019-11-21 21:09:40 +0700

    updated

--- app/Http/Controllers/Guru/ScheduleController.php
+++ app/Http/Controllers/Guru/ScheduleController.php
@@ -113,7 +113,7 @@ class ScheduleController extends Controller
             ->where('expired', 1)
             ->orWhere('canceled', 1)
             ->orWhere('finish', 1)
-            ->orderBy('created_at', 'desc')
+            ->orderBy('updated_at', 'desc')
             ->with('requester')
             ->paginate($request->per_page);
 
