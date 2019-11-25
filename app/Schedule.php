db128364899dc4cdd5f3d923c0097af89cbab5c3
AldiSatrioAji <aldis85@yahoo.com>
2019-11-25 12:42:07 +0700

    catatan konsel

--- app/Schedule.php
+++ app/Schedule.php
@@ -52,7 +52,7 @@ class Schedule extends Model
 
     public function catatan()
     {
-        return $this->belongsTo('\App\CatatanKonseling', 'schedule_id'. 'id');
+        return $this->belongsTo('\App\CatatanKonseling', 'id'. 'schedule_id');
     }
 
     public function getTimeAttribute()
