8a648d30ec7227dd9d5038c385f06efa5aeff6a7
AldiSatrioAji <aldis85@yahoo.com>
2019-11-29 23:57:03 +0700

    security improvement

--- app/Http/Controllers/Guru/ScheduleController.php
+++ app/Http/Controllers/Guru/ScheduleController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Response;
 use App\Schedule;
+use Carbon\Carbon;
 
 class ScheduleController extends Controller
 {
