9a95baf438ae6d89e55310b5b83d5510092eed58
AldiSatrioAji <aldis85@yahoo.com>
2019-11-29 15:20:31 +0700

    kfr

--- routes/web.php
+++ routes/web.php
@@ -127,6 +127,7 @@ $router->group(['prefix'=>'v1/api'], function () use ($router) {
                 $router->get('schedule/jadwal/pending', 'ScheduleController@jadwalPending');
                 $router->get('schedule/jadwal/aktif', 'ScheduleController@jadwalAktif');
                 $router->get('schedule/obrolan/pending', 'ScheduleController@obrolanPending');
+                $router->get('schedule/obrolan/aktif', 'ScheduleController@obrolanAktif');
             });
 
         });
