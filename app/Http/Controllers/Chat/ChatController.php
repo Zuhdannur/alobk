bacdf0028a161dfd49e1337663a4750d65ca266c
AldiSatrioAji <aldis85@yahoo.com>
2019-12-08 18:43:31 +0700

    send notification

--- app/Http/Controllers/Chat/ChatController.php
+++ app/Http/Controllers/Chat/ChatController.php
@@ -1,6 +1,7 @@
 <?php namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Response;
 use Berkayk\OneSignal\OneSignalClient;
 
