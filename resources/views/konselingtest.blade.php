bfbb68958c7c1575683e46a9305c23d76a92f6ce
AldiSatrioAji <aldis85@yahoo.com>
2019-12-10 06:55:51 +0700

    test

--- resources/views/konselingtest.blade.php
+++ resources/views/konselingtest.blade.php
@@ -44,8 +44,8 @@
 				<td>{{$p->consultant->name}}</td>
 				<td>{{$p->title}}</td>
 				<td>{{$p->desc}}</td>
-                <td>{{ $p->type_schedule == 'direct' ? 'Lokasi: '.$p->location."; Waktu: ".$p->time: '-'}}</td>
-                <td>{{ @foreach(json_decode(Firebase::get('/room/messages/'.$p->id,['print'=> 'pretty']), 1) as $key => $val) $val['message'] }}</td>
+                <td>{{$p->type_schedule == 'direct' ? 'Lokasi: '.$p->location."; Waktu: ".$p->time:'-'}}</td>
+                <td>@foreach(json_decode(Firebase::get('/room/messages/'.$p->id,['print'=> 'pretty']), 1) as $key => $val) {{ $val['message'] }}</td>
 				<td>{{ !empty($p->feedback) ? $p->feedback->komentar:'-' }}</td>
 				<td>{{ !empty($p->feedback) ? $p->feedback->rating."/5":'-' }}</td>
 			</tr>
