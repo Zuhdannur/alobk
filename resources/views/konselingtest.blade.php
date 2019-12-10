da2cd91e6540fe5693a6c29ca510dd1bb596abf4
AldiSatrioAji <aldis85@yahoo.com>
2019-12-10 07:15:00 +0700

    test

--- resources/views/konselingtest.blade.php
+++ resources/views/konselingtest.blade.php
@@ -46,8 +46,9 @@
 				<td>{{$p->desc}}</td>
                 <td>{{$p->type_schedule == 'direct' ? 'Lokasi: '.$p->location."; Waktu: ".$p->time:'-'}}</td>
                 <td>
-                    @if(is_array(json_decode(Firebase::get('/room/messages/'.$p->id,['print'=> 'pretty']), 1)) || is_object(json_decode(Firebase::get('/room/messages/'.$p->id,['print'=> 'pretty']), 1))
-                        @foreach(json_decode(Firebase::get('/room/messages/'.$p->id,['print'=> 'pretty']), 1) as $key => $val) 
+                    <?php $data = json_decode(Firebase::get('/room/messages/'.$p->id,['print'=> 'pretty']), 1); ?>
+                    @if(is_array($data) || is_object($data)
+                        @foreach($data) as $key => $val) 
                             {{ $val['message'] }} 
                         @endforeach
                     @endif
