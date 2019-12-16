671793d8145cd5c6bd772d0060fdbccbb658c068
AldiSatrioAji <aldis85@yahoo.com>
2019-12-16 11:14:07 +0700

    type schedule

--- app/Http/Controllers/Siswa/ScheduleController.php
+++ app/Http/Controllers/Siswa/ScheduleController.php
@@ -342,8 +342,8 @@ class ScheduleController extends Controller
    }
 
     public function update() {
-        $schedule1 = $this->schedule->where('type_schedule', 'daring')->update(['type_schedule', 'realtime1']);
-        $schedule2 = $this->schedule->where('type_schedule', 'realtime')->update(['type_schedule', 'daring1']);
+        $schedule1 = Schedule::where('type_schedule', 'daring')->update(['type_schedule', 'realtime1']);
+        $schedule2 = Schedule::where('type_schedule', 'realtime')->update(['type_schedule', 'daring1']);
 
         if($schedule1 && $schedule2) {
             return Response::json(['message' => 'Berhasil'], 200);
