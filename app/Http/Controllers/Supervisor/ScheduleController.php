05ec23001d85374eee144a35f8a36b48bf493df4
AldiSatrioAji <aldis85@yahoo.com>
2019-12-18 18:05:23 +0700

    fix

--- app/Http/Controllers/Supervisor/ScheduleController.php
+++ app/Http/Controllers/Supervisor/ScheduleController.php
@@ -105,9 +105,9 @@ class ScheduleController extends Controller
         // }
 
         // else if($request->tipe == 'pending') {
-            $direct = Schedule::requesterSameSchool()->consultantSameSchool()->createdToday()->isPending()->isDirect()->count();
-            $realtime = Schedule::requesterSameSchool()->consultantSameSchool()->createdToday()->isPending()->isRealtime()->count();
-            $daring = Schedule::requesterSameSchool()->consultantSameSchool()->createdToday()->isPending()->isDaring()->count();
+            $direct = Schedule::requesterSameSchool()->consultantSameSchool()->createdToday()->isPending()->count();
+            $realtime = Schedule::requesterSameSchool()->consultantSameSchool()->createdToday()->isPending()->count();
+            $daring = Schedule::requesterSameSchool()->consultantSameSchool()->createdToday()->isPending()->count();
         // }
 
         // else if($request->tipe == 'finish') {
