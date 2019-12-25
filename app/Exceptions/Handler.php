292ddae59fda61bc906481b39ba917e640201675
AldiSatrioAji <aldis85@yahoo.com>
2019-12-25 18:04:32 +0700

    upload

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -50,17 +50,17 @@ class Handler extends ExceptionHandler
             switch ($exception->getStatusCode()) {
 
                 case '403':
-                    return \Response::json(['message' => "Unauthorized."]);
+                    return Response::json(['message' => "Unauthorized."]);
                     break;
 
                 // internal error
                 case '500':
-                    return \Response::json(['message' => "Unauthorized."]);
+                    return Response::json(['message' => "Unauthorized."]);
                     break;
     
                 // not found
                 case '404':
-                    return \Response::json(['message' => "Unauthorized."]);
+                    return Response::json(['message' => "Unauthorized."]);
                     break;
     
                 default:
