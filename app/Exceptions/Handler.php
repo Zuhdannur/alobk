185a01537d5405df0a6a1f7c8f53cdf70a06f25f
AldiSatrioAji <aldis85@yahoo.com>
2019-12-25 18:02:06 +0700

    upload

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -45,6 +45,29 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
+        if ($this->isHttpException($exception)) {
+            switch ($exception->getStatusCode()) {
+
+                case '403':
+                    return \Response::json(['message' => "Unauthorized."]);
+                    break;
+
+                // internal error
+                case '500':
+                    return \Response::view('error404',array(),500);
+                    break;
+    
+                // not found
+                case '404':
+                    return \Response::view('error404',array(),404);
+                    break;
+    
+                default:
+                    return $this->renderHttpException($e);
+                    break;
+            }
+        } else {
+            return parent::render($request, $exception);
+        }
     }
 }
