9fc6b03cec2a7b78b301ac584217808a1da1f0c5
Zuhdan Nur <pczuhdan33@gmail.com>
2020-09-14 20:36:57 +0700

    update

--- app/Http/Controllers/ImportDataController.php
+++ app/Http/Controllers/ImportDataController.php
@@ -9,40 +9,48 @@ use Illuminate\Support\Facades\File;
 
 class ImportDataController extends Controller
 {
-    public function import(Request $request) {
+    public function import(Request $request)
+    {
 
 
         //Move File
         $file = $request->upload->getClientOriginalExtension();
 
-        $file = uniqid().'.'.$file;
-        $path = 'uploads'.DIRECTORY_SEPARATOR.'files'.DIRECTORY_SEPARATOR;
+        if ($file == "xlsx" || $file == "csv") {
 
-        $destinationPath = public_path($path);
+            $file = uniqid() . '.' . $file;
+            $path = 'uploads' . DIRECTORY_SEPARATOR . 'files' . DIRECTORY_SEPARATOR;
 
-        File::makeDirectory($destinationPath,0777,true,true);
-        $request->upload->move($destinationPath,$file);
+            $destinationPath = public_path($path);
+
+            File::makeDirectory($destinationPath, 0777, true, true);
+            $request->upload->move($destinationPath, $file);
 
 
-        $baseUrl = getenv('URL_IMPORT_DATA');
+            $baseUrl = getenv('URL_IMPORT_DATA');
 
-        $client = new Client();
-        $request = $client->request('POST',$baseUrl.'import',[
-            'multipart' => [
-                [
-                    'name' => 'upload',
-                    'contents' => fopen($destinationPath.$file,'r')
+            $client = new Client();
+            $request = $client->request('POST', $baseUrl . 'import', [
+                'multipart' => [
+                    [
+                        'name' => 'upload',
+                        'contents' => fopen($destinationPath . $file, 'r')
+                    ]
                 ]
-            ]
+            ]);
+
+            unlink($destinationPath . $file);
+
+            $return = [
+                "code" => $request->getStatusCode(),
+                "message" => "Imported"
+            ];
+
+            return response()->json($return);
+        }
+        return response()->json([
+            "message" => "extension tidak sesuai , format harus xlsx atau csv",
+            "code" => 400
         ]);
-
-        unlink($destinationPath.$file);
-
-        $return = [
-            "code" => $request->getStatusCode(),
-            "message" => "Imported"
-        ];
-
-        return response()->json($return);
     }
 }
